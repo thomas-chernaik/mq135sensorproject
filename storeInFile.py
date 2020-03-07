a6174e0a58c6562503dd0f1c4214c3fc26bee4a6
pi-club-daos <14chernaikt.student@daos.uk>
2020-03-07 15:37:50 +0000

    semi working

--- storeInFile.py
+++ storeInFile.py
@@ -1,9 +1,9 @@
 import time
 import datetime
-import bluetoothgetLocation
+import random
 #import getReadings
 #getReadings.init()
-num = 0
+num = 0.0
 #location = input("please enter the location of the device")#need to find a better way of getting the location
 
 while True:
@@ -19,20 +19,26 @@ def getData():
     #this function is just for testing#
     ###################################
     global num
-    num += 1
+    num = random.random()
     return str(num)
 def store(file, freq):
+    i=0
     while True:
         with open(file, "a") as myfile:
-            with open("location.txt", "r") as myfile:
-                location = myfile.read()
+            with open("location.txt", "r") as myfile1:
+                location = myfile1.read()
             #var = getReadings.read() + "," + datetime.datetime.now() + "\n"
             #print(str(datetime.datetime.now()).split(":")[0]) this gives the date and hour
             now = datetime.datetime.now()
-            strnow = now.strftime("%Y-%m-%d-%H-%M-%s")
-            print(strnow)
-            var = getData() + "," + location + ",50.0,-0.1,"  + strnow + "\n"
+            strnow = now.strftime("%Y-%m-%d-%H-%M-%S")
+            #print(strnow)
+            #var = getData() + "," + location + ",50.0,-0.1,"  + strnow + "\n"
+            gps = "50.0,-0.1"
+            var = "%s,%s,%s,%s\n" %(strnow, location, gps, getData())#change to getreadings
             myfile.write(var)
             time.sleep(1/freq)
+            i+=1
+            if i==1000:
+               break
 
 store("hello.txt", 1)
